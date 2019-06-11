fef89b9c6def9cc568c760135469bf648d716e91
Franciel1990 <franciel_b@hotmail.com>
2019-06-11 20:59:59 -0300

    Msg Tchau tchau

--- src/application/ProgramGit.java
+++ src/application/ProgramGit.java
@@ -4,5 +4,6 @@ public class ProgramGit {
 	public static void main(String[] args) {
 		System.out.println("Bom dia!");
 		System.out.println("Boa noite!");
+		System.out.println("Tchau tchau!");
 	}
 }
